2ef876dd28f635d7d120a6dfd37184a5c80af7ec
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-09 00:05:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,7 @@
 #write your code here
 
 def countdown(number)
-  number = 11
-  while number > 0
-    number -= 1
-    puts "#{number} SECOND(S)"
-  end
-  
-  puts "HAPPY NEW YEAR!"
-  
+
 end
 
 #def countdown_with_sleep(integer)
