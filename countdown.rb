9e4af5463e57d576a1f21a928271cb7a574f6f5e
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-09 00:52:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,7 +8,6 @@ def countdown(number)
     number -= 1
   end
   
-  puts "HAPPY NEW YEAR!"
   return "HAPPY NEW YEAR!"
   
 end
@@ -22,7 +21,8 @@ def countdown_with_sleep(number)
     sleep(1)
   end
   
-  puts "HAPPY NEW YEAR!"
   return "HAPPY NEW YEAR!"
   
 end
+
+puts "HAPPY NEW YEAR!"
